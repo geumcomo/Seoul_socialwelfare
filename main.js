d3a4612e5c0591f1c584b3ac20e681811f23e128
geummo <kummo99@naver.com>
2024-03-19 20:11:48 +0900

    first commit

--- main.js
+++ main.js
@@ -1,18 +1,29 @@
-import './style.css';
-import Map from 'ol/Map.js';
-import OSM from 'ol/source/OSM.js';
-import TileLayer from 'ol/layer/Tile.js';
-import View from 'ol/View.js';
-
-const map = new Map({
-  target: 'map',
-  layers: [
-    new TileLayer({
-      source: new OSM(),
-    }),
-  ],
-  view: new View({
-    center: [0, 0],
-    zoom: 2,
-  }),
+var mapView = new ol.View({
+  center: ol.proj.fromLonLat([126.978, 37.566]),
+  zoom: 10
 });
+
+var map = new ol.Map({
+  target: 'map',
+  view: mapView
+});
+
+var osmTile = new ol.layer.Tile({
+  title: 'Open Street Map',
+  visible: true,
+  source: new ol.source.OSM()
+});
+
+map.addLayer(osmTile);
+
+var seoulTile = new ol.layer.Tile({
+  title: "Seoul sw",
+  source: new ol.source.TileWMS({
+    url: 'http://localhost:8089/geoserver/GISSimplified/wms?service=WMS',
+    params: {'LAYERS': 'GISSimplified:Seoul_sw3857', 'TILED': true},
+    serverType: 'geoserver',
+    visible: true
+  })
+});
+
+map.addLayer(seoulTile);
